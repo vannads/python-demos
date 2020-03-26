c2dbf298b9a3aa16ef489a0a1ad8ae237adc1389
Sathisan Vannadil <vannads@gmail.com>
2020-03-26 23:32:13 +0530

    Journal App

---
+++ journal_app/program.py
@@ -0,0 +1,15 @@
+#
+def print_header():
+    print("-----------------------------")
+    print("        JOURNAL APP")
+    print("-----------------------------")
+
+def run_event_loop():
+    print("What do you want to do with your journal? : ")
+    pass
+
+def main():
+    print_header()
+    run_event_loop()
+
+main()
